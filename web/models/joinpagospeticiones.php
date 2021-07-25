750bf79f2b2d1d188ca815510ed04c686a283e62
David Cordoba <davbaster@gmail.com>
2021-07-25 16:17:30 -0600

    model joinpagospeticiones.php created

---
+++ web/models/joinpagospeticiones.php
@@ -0,0 +1,86 @@
+<?php
+
+class JoinPagosPeticionesModel extends Model {
+
+    private $pagoId;
+    private $title;
+    private $amount;
+    private $categoryId; //$peticionPagoId;
+    private $date;
+    private $userId;
+    private $nameCategory;
+    private $color;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    //
+    public function getAll($userId){
+        $items = [];
+        try{
+          //regresa la union de donde la peticion_pago_id es igual al id de la tabla peticiones_pago con el usuario dado $userId
+          //$query = $this->prepare('SELECT expenses.id as expense_id, title, category_id, amount, date, id_user, categories.id, name, color  FROM expenses INNER JOIN categories WHERE expenses.category_id = categories.id AND expenses.id_user = :userid ORDER BY date');
+            $query = $this->prepare('SELECT pagos.id as pago_id, title, peticion_pago_id, amount, date, id_user, peticiones_pago.id, name, color  FROM pagos INNER JOIN peticiones_pago WHERE pagos.peticion_pago_id = peticiones_pago.id AND pagos.id_user = :userId ORDER BY date');
+            $query->execute(["userId" => $userId]);
+
+
+            while($p = $query->fetch(PDO::FETCH_ASSOC)){
+                $item = new JoinPagosPeticionesModel();
+                $item->from($p);
+                array_push($items, $item);
+            }
+
+            return $items;
+
+        }catch(PDOException $e){
+            echo $e;
+        }
+    }
+
+
+    //
+    public function from($array){
+        $this->expenseId = $array['pago_id'];
+        $this->title = $array['title'];
+        $this->peticionPagoId = $array['peticion_pago_id'];
+        $this->amount = $array['amount'];
+        $this->date = $array['date'];
+        $this->userId = $array['id_user'];
+        $this->nameCategory = $array['name'];
+        $this->color = $array['color'];
+    }
+
+
+    //
+    public function toArray(){
+        $array = [];
+        $array['id'] = $this->pagoId;
+        $array['title'] = $this->title;
+        $array['peticion_pago_id'] = $this->peticionPagoId;
+        $array['amount'] = $this->amount;
+        $array['date'] = $this->date;
+        $array['id_user'] = $this->userId;
+        $array['name'] = $this->nameCategory;
+        $array['color'] = $this->color;
+
+        return $array;
+    }
+
+
+    public function getPagoId(){return $this->pagoId;}
+    public function getTitle(){return $this->title;}
+    public function getPeticionPagoId(){return $this->peticionPagoId;}
+    public function getAmount(){return $this->amount;}
+    public function getDate(){return $this->date;}
+    public function getUserId(){return $this->userId;}
+    public function getNameCategory(){return $this->nameCategory;}
+    public function getColor(){return $this->color;}
+
+    
+}
+
+?>
