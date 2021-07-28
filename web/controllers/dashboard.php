796afa39a74d21b92ccb788f9a8c230b3e635112
David Cordoba <davbaster@gmail.com>
2021-07-27 21:04:11 -0600

    Working in dashboard controller

--- web/controllers/dashboard.php
+++ web/controllers/dashboard.php
@@ -2,23 +2,76 @@
 
 class Dashboard extends SessionController{
 
+    private $user;
+
     function __construct(){
         parent::__construct();
-        error_log('Dashboard::construct -> inicio de Dashboard');
-        
+
+        $this->user = $this->getUserSessionData();
+        error_log("Dashboard::constructor() ");
     }
 
+
+    //
     function render(){
-        error_log('Dashboard::render -> Carga el index del Dashboard');
-        $this->view->render('dashboard/index');
+        error_log("Dashboard::RENDER() ");
+        $pagosModel             = new PagosModel();
+        $pagos                  = $this->getPagos(5);
+        $totalThisMonth         = $pagosModel->getTotalAmountThisMonth($this->user->getId());
+        $maxExpensesThisMonth   = $pagosModel->getMaxExpensesThisMonth($this->user->getId());
+        $peticionesPagos        = $this->getPeticionesPagos();
+
+        $this->view->render('dashboard/index', [
+            'user'                 => $this->user,
+            'pagos'                => $pagos,
+            'totalAmountThisMonth' => $totalThisMonth,
+            'maxPagosThisMonth'    => $maxPagosThisMonth,
+            'peticionesPagos'      => $peticionesPagos
+        ]);
     }
 
-    //getPagos
-    public function getExpenses(){
 
-
-
+    //obtiene la lista de expenses y $n tiene el número de expenses por transacción
+     private function getPagos($n = 0){
+        if($n < 0) return NULL;
+        error_log("Dashboard::getPagos() id = " . $this->user->getId());
+        $pagos = new PagosModel();
+        return $pagos->getByUserIdAndLimit($this->user->getId(), $n);   
     }
+
+
+    //getCategories
+    function getPeticionesPagos(){
+        $res = [];
+        $peticionesPagoModel = new PeticionesPagoModel();
+        $pagosModel = new PagosModel();
+
+        $peticiones = $peticionesPagoModel->getAll();
+
+        foreach ($peticiones as $p) {
+            $peticionesArray = [];
+            //obtenemos la suma de amount de expenses por categoria
+            $total = $pagosModel->getTotalByCategoryThisMonth($p->getId(), $this->user->getId());
+            // obtenemos el número de expenses por categoria por mes
+            $numberOfPagos = $pagosModel->getNumberOfExpensesByCategoryThisMonth($p->getId(), $this->user->getId());
+            
+            if($numberOfPagos > 0){
+                $peticionesArray['total'] = $total;
+                $peticionesArray['count'] = $numberOfPagos;
+                $peticionesArray['peticion'] = $p;
+                array_push($res, $peticionesArray);
+            }
+            
+        }
+        return $res;
+    }
+
+    //getExpenses
+    // public function getPagos(){
+
+
+
+    // }
 
     //getPlanillas
     public function getCategories(){
