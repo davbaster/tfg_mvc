6d6272ae13a88b464d77525311fc906ea7a0f28f
David Cordoba <davbaster@gmail.com>
2021-09-05 12:35:50 -0600

    {

--- web/views/peticionespago/index.php
+++ web/views/peticionespago/index.php
@@ -15,88 +15,84 @@
 <div id="main-container">
     <div id="user-container" class="container">
 
-        <button class="tablink" onclick="openPage('Home', this, 'red')">Adelantos</button>
-        <button class="tablink" onclick="openPage('News', this, 'green')" id="defaultOpen">Pendientes</button>
+        <button class="tablink" onclick="openPage('Adelantos', this, 'red') " id="defaultOpen">Adelantos </button>
+        <button class="tablink" onclick="openPage('News', this, 'green')" >Pendientes</button>
         <button class="tablink" onclick="openPage('Contact', this, 'blue')">Aprobadas</button>
         <button class="tablink" onclick="openPage('Historial', this, 'orange')">Historial</button>
 
-        <div id="Home" class="tabcontent">
-            <h3>Adelantos de pago</h3>
-            <!-- PAGINA ADELANTO DE PAGOS -->
-            <div id="Adelantos" class="tabcontent">
+        <div id="Adelantos" class="tabcontent">
+                  
+            <div id="history-container" class="container">
+                
+                <div id="history-options">
+                    <h2>Prestamos</h2>
+                    <!--//TODO mejora: dividir el filtro en a;o, mes, dia  -->
+                    <!-- //TODO mejora: hacer que los filtros sean cruzados entre si -->
                     
-                <div id="history-container" class="container">
-                    
-                    <div id="history-options">
-                        <h2>Historial de Planillas</h2>
-                        <!--//TODO mejora: dividir el filtro en a;o, mes, dia  -->
-                        <!-- //TODO mejora: hacer que los filtros sean cruzados entre si -->
-                        
-                        <div id="filters-container-prestamos">
-                            <div class="filter-container">
-                                <select id="sdate-prestamos" class="custom-select">
-                                    <!-- va php code v11min51 -->
-                                    <option value="">Ver todas las fechas</option>
-                                        <?php
-                                            $options = $fechas;
-                                            foreach ($options as $option) {
-                                                echo "<option value=$option >".$option."</option>";
-                                            }
-                                        ?>
-                                </select>
-                            </div>
-    
-                            <div class="filter-container">
-                                <select id="scontratista-prestamos" class="custom-select">
-                                    <option value="">Todos los Contratistas</option>
-                                        <?php
-                                            $options = $contratistas;
-                                            foreach ($options as $option) {
-                                                // $valor = $option['nombre'] .' '. $option['apellido1'] ;
-                                                echo "<option value=$option >".$option ."</option>";
-                                            }
-                                        ?>
-                                    
-                                </select>
-                            </div>
-    
-                            <div class="filter-container">
-                                <select id="sestado-prestamo" class="custom-select">
-                                    <option value="">Todos los estados</option>
-    
-                                    <option value="autorizado" >Autorizado</option>;
-                                        <option value="pendiente" >Pendiente</option>;
-                                        <option value="pagado" >Pagado</option>;
-    
-                                    
-                                </select>
-                            </div>
-                        </div>   
-                    </div>
-                    
-                    <div id="table-container-prestamos">
-                        <table width="100%" cellpadding="0">
-                            <thead>
-                                <tr>
-                                <th data-titulo="Numero:">ID</th>
-                                <th data-titulo="Contratista:" width="20%">Contratista</th>
-                                <th data-titulo="Fecha:">Fecha</th>
-                                <th data-titulo="Monto:">Cantidad</th>
-                                <th data-titulo="Estado:">Estado</th>
-                                <th>Acciones</th>
-                                </tr>
-                            </thead>
-                            <tbody id="databody-prestamos">
+                    <div id="filters-container">
+                        <div class="filter-container">
+                            <select id="sdate" class="custom-select">
+                                <!-- va php code v11min51 -->
+                                <option value="">Ver todas las fechas</option>
+                                    <?php
+                                        $options = $fechas;
+                                        foreach ($options as $option) {
+                                            echo "<option value=$option >".$option."</option>";
+                                        }
+                                    ?>
+                            </select>
+                        </div>
+
+                        <div class="filter-container">
+                            <select id="scontratista" class="custom-select">
+                                <option value="">Todos los Contratistas</option>
+                                    <?php
+                                        $options = $contratistas;
+                                        foreach ($options as $option) {
+                                            // $valor = $option['nombre'] .' '. $option['apellido1'] ;
+                                            echo "<option value=$option >".$option ."</option>";
+                                        }
+                                    ?>
                                 
-                            </tbody>
-                        </table>
-                    </div>
-                    
+                            </select>
+                        </div>
+
+                        <div class="filter-container">
+                            <select id="sestado" class="custom-select">
+                                <option value="">Todos los estados</option>
+
+                                <option value="autorizado" >Autorizado</option>;
+                                    <option value="pendiente" >Pendiente</option>;
+                                    <option value="pagado" >Pagado</option>;
+
+                                
+                            </select>
+                        </div>
+                    </div>   
                 </div>
-                                            
-            </div><!--FIN DE ADELANTOS DE PAGO  -->
+                
+                <div id="table-container">
+                    <table width="100%" cellpadding="0">
+                        <thead>
+                            <tr>
+                            <th >ID</th>
+                            <th data-titulo="Contratista:" width="20%">Contratista</th>
+                            <th data-titulo="Fecha:">Fecha</th>
+                            <th data-titulo="Monto:">Cantidad</th>
+                            <th data-titulo="Estado:">Estado</th>
+                            <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody id="databody">
+                            
+                        </tbody>
+                    </table>
+                </div>
+                
+            </div>
+            <script src="public/js/peticiones_prestamos.js"></script>         
+        </div><!--FIN DE ADELANTOS DE PAGO/PRESTAMOS  -->
             
-        </div>
 
         <div id="News" class="tabcontent">
         <h3>Planillas pendientes de revision</h3>
@@ -179,10 +175,11 @@
                 </div>
                 
             </div>
+            <script src="public/js/peticionespago.js"></script>
                                         
         </div><!--FIN DE HISTORIAL  -->
 
-        <script src="public/js/peticionespago.js"></script>
+        
 
         </div>
 
